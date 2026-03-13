c04ddc23368c029b91e73210549f054065ea8988
Simon Eves <seves@nvidia.com>
2026-03-13 13:25:45 -0700

    Match concat PR implementation

--- velox/experimental/cudf/expression/ExpressionEvaluator.cpp
+++ velox/experimental/cudf/expression/ExpressionEvaluator.cpp
@@ -1749,9 +1749,9 @@ class ConcatFunction : public CudfFunction {
     VELOX_CHECK_GE(numInputs_, 2, "concat expects at least 2 inputs");
 
     // Scan inputs for literals and store strings in map by input index.
-    // Capture the size of the first non-literal input column and validate all others.
     for (size_t i = 0; i < numInputs_; ++i) {
-      if (auto constant = std::dynamic_pointer_cast<ConstantExpr>(expr->inputs()[i])) {
+      if (auto constant =
+              std::dynamic_pointer_cast<ConstantExpr>(expr->inputs()[i])) {
         inputIndexToLiteral_[i] = constant->value()->toString(0);
       }
     }
@@ -1761,10 +1761,18 @@ class ConcatFunction : public CudfFunction {
       std::vector<ColumnOrView>& inputColumns,
       rmm::cuda_stream_view stream,
       rmm::device_async_resource_ref mr) const override {
+    // Validate sizes.
+    VELOX_CHECK_EQ(
+        inputColumns.size() + inputIndexToLiteral_.size(),
+        numInputs_,
+        "Unexpected number of input columns");
+
     // If there is at least one input column, fetch its size as the output size.
-    // If there are no input columns, the output size will be 1.
-    size_t outputSize = inputColumns.empty() ? 1u : asView(inputColumns[0]).size();
-    
+    // If there are no input columns, this means that all the inputs are
+    // literals, and the output size will be 1.
+    const size_t outputSize =
+        inputColumns.empty() ? 1u : asView(inputColumns[0]).size();
+
     // Iterate the inputs, building a vector of column views, either a literal
     // from the map, or the next input column. We also keep a vector of the
     // columns created for literals, so that they persist while their views
@@ -1780,9 +1788,10 @@ class ConcatFunction : public CudfFunction {
         columnViews.push_back(asView(column));
       } else {
         // Create a column of the literal repeated for the entire output size.
-        auto const literal = it->second;
+        auto const& literal = it->second;
         cudf::string_scalar scalar(literal, true, stream, mr);
-        auto col = cudf::make_column_from_scalar(scalar, outputSize, stream, mr);
+        auto col =
+            cudf::make_column_from_scalar(scalar, outputSize, stream, mr);
         columnViews.push_back(col->view());
         literalColumns.emplace_back(std::move(col));
       }
@@ -1790,7 +1799,13 @@ class ConcatFunction : public CudfFunction {
 
     // Concatenate the columns, nulls as empty strings, no separators.
     cudf::string_scalar emptyString("");
-    return cudf::strings::concatenate(cudf::table_view(columnViews), emptyString, emptyString, cudf::strings::separator_on_nulls::YES, stream, mr);
+    return cudf::strings::concatenate(
+        cudf::table_view(columnViews),
+        emptyString,
+        emptyString,
+        cudf::strings::separator_on_nulls::YES,
+        stream,
+        mr);
   }
 
  private:
@@ -2202,7 +2217,7 @@ bool registerBuiltinFunctions(const std::string& prefix) {
       {FunctionSignatureBuilder()
            .returnType("varchar")
            .argumentType("varchar")
-           .argumentType("varchar")
+           .variableArity("varchar")
            .build()});
 
   // No prefix because switch and if are special form
